5cd38bff588429bea6f211076d0cc0635573e8bf
MOTY <50269513+MOTY12@users.noreply.github.com>
2021-08-20 15:40:32 +0100

    update te database of the

--- model/course.js
+++ model/course.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 
 const courseSchema = new mongoose.Schema({
+  
     title: {
         type: String
     },
     author: {
-        type: String
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
     },
     description: {
         type: String
