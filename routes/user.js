f15808222eacf4973d7b8eb498308c73ed4d335b
MOTY <50269513+MOTY12@users.noreply.github.com>
2021-08-21 18:03:21 +0100

    fsggfhg

--- routes/user.js
+++ routes/user.js
@@ -41,12 +41,12 @@ router.get('/user/:id', async(req, res) => {
 router.get('/crosscheck', async(req, res) => {
 
     var smtpTransport = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        service: "Gmail",
+        host: 'smtp-relay.sendinblue.com',
+//         service: "Gmail",
         port: "587",
         auth: {
             user: "mukhtarapril2000@gmail.com",
-            pass: "opeyemi2944"
+            pass: "UBYDRcQv19W0C5t2"
         }
     });
     var mailOptions = {
