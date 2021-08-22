2b0043a835e01400c8bdf41956cd0e4babbd4f89
MOTY <50269513+MOTY12@users.noreply.github.com>
2021-08-22 17:43:49 +0100

    update course

--- routes/course.js
+++ routes/course.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const Course = require('../model/course')
+const multer = require("multer");
+const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
 const router = express()
 
 router.get("/courses", async(req, res) => {
@@ -17,27 +20,107 @@ router.get("/course/:id", async(req, res) => {
     }
     res.status(200).json(courses)
 })
-router.post("/course", async(req, res) => {
-    // var adminId = req.userId
-    const course = new Course({
-        // AdminId: adminId,
-        title: req.body.title,
-        author: req.body.author,
-        description: req.body.description,
-        audiof: req.body.audiof,
-        image: req.body.image
-    })
-    const courses = await course.save()
 
-    if (!courses) {
-        res.send("Unsuccessfull!!!. pls try again later")
-    } else {
-        res.send(courses)
+
+router.post("/course", async(req, res) => {
+
+    // SEND FILE TO CLOUDINARY
+    cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    // var adminId = req.userId
+
+    try {
+
+        const storage = multer.diskStorage({
+            filename: (req, file, cb) => {
+                const fileExt = file.originalname.split(".").pop();
+                const filename = `${new Date().getTime()}.${fileExt}`;
+                cb(null, filename);
+            },
+        });
+
+        // Filter the file to validate if it meets the required audio extension
+        const fileFilter = (req, file, cb) => {
+            if (file.mimetype === "audio/mp3" || file.mimetype === "audio/mpeg") {
+                cb(null, true);
+            } else {
+                cb({
+                        message: "Unsupported File Format",
+                    },
+                    false
+                );
+            }
+        };
+
+        // Set the storage, file filter and file size with multer
+        const upload = multer({
+            storage,
+            limits: {
+                fieldNameSize: 200,
+                fileSize: 5 * 1024 * 1024,
+            },
+            fileFilter,
+        }).single("audio");
+
+        // upload to cloudinary
+        upload(req, res, (err) => {
+            if (err) {
+                return res.send(err);
+            }
+
+            const { path } = req.file; // file becomes available in req at this point
+
+            const fName = req.file.originalname.split(".")[0];
+            cloudinary.uploader.upload(
+                path, {
+                    resource_type: "raw",
+                    public_id: `AudioUploads/${fName}`,
+                },
+
+                // Send cloudinary response or catch error
+                (err, audio) => {
+                    if (err) return res.send(err);
+
+                    fs.unlinkSync(path);
+                    const audiourl = audio.url
+
+                    const course = new Course({
+                        // AdminId: adminId,
+                        title: req.body.title,
+                        author: req.body.author,
+                        description: req.body.description,
+                        audiof: audiourl,
+                        images: req.body.images
+                    })
+
+
+                    course.save((err, course) => {
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            if (!course) {
+                                res.send("Unsuccessfull!!!. pls try again later")
+                            } else {
+                                res.send(course)
+                            }
+                        }
+
+                    });
+                });
+        })
+    } catch (err) {
+        console.log(err);
     }
 
 
     // res.send('hello world')
 })
+
+
+
 
 router.put("/updatecourse/:id", async(req, res) => {
     const updatecourse = await Course.findByIdAndUpdate(req.params.id, {
