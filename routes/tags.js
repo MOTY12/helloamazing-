fa9f025ff52f9af4fd742db67190d2936e7e1c57
MOTY <50269513+MOTY12@users.noreply.github.com>
2021-08-25 16:02:47 +0100

    moty

--- routes/tags.js
+++ routes/tags.js
@@ -1,44 +1,44 @@
 const express = require('express').Router
-const tagSchema = require('../model/tag')
+const tagSchema = require('../model/tags')
 const router = express()
 
 
 // get new training
-router.get('/tag', async (req, res)=>{
-        const taglist = await tagSchema.find()
+router.get('/tag', async(req, res) => {
+    const tagslist = await tagSchema.find().split("-").join(" ")
     if (!taglist) {
         res.status(500).json({
             message: "No author found "
         })
     }
-    
-    res.status(200).json({data: taglist})
-})        
+
+    res.status(200).json({ data: taglist })
+})
 
 
 //insert new training
-router.post('/tag', async (req, res) => {
-        const tagField = new tagSchema({
-            tag: req.body.tag
-        })
-       const fields= await tagField.save()
-        if(!fields)
-        return res.status(404).send({message: 'Tag is not created'})
-    
-        res.send({data: fields})
+router.post('/tag', async(req, res) => {
+    const tagField = new tagSchema({
+        tag: req.body.tag
+    })
+    const fields = await tagField.save()
+    if (!fields)
+        return res.status(404).send({ message: 'Tag is not created' })
+
+    res.send({ data: fields })
 
 })
 
 
-router.get('/tag/:id', async(req,res)=>{
+router.get('/tag/:id', async(req, res) => {
     const tagid = await tagSchema.findById(req.params.id)
-    if(!tagid){
-    res.status(500).json({
-        success: false,
-        message: "No data for the tag requested"
-    })
-}
-    res.status(200).json({data: tagid})
+    if (!tagid) {
+        res.status(500).json({
+            success: false,
+            message: "No data for the tag requested"
+        })
+    }
+    res.status(200).json({ data: tagid })
 })
 
 
@@ -46,27 +46,30 @@ router.get('/tag/:id', async(req,res)=>{
 //delete author
 router.delete('/tag/:id', async(req, res) => {
     const tag = await tagSchema.findByIdAndRemove(req.params.id)
-    .then(tag => {if(tag){
-       return res.status(200).json({
-           success: true,
-           message: "Successfully deleted"
-       })
-    }else{
-        return res.status(200).json({
-            success: false,
-            message: "the author cannot be found"
+        .then(tag => {
+            if (tag) {
+                return res.status(200).json({
+                    success: true,
+                    message: "Successfully deleted"
+                })
+            } else {
+                return res.status(200).json({
+                    success: false,
+                    message: "the author cannot be found"
+                })
+            }
+        }).catch(err => {
+            return res.status(400).json({
+                success: false,
+                error: err
+            })
         })
-    }
-    }).catch(err=>{
-          return res.status(400).json({
-            success: false,
-            error: err})
-    }) })
-    
+})
 
 
- 
 
- 
+
+
+
 
 module.exports = router
