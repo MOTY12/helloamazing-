2fbd8abaf70395c0ddd4d166e1fdcb5e34b16913
MOTY <50269513+MOTY12@users.noreply.github.com>
2021-08-26 15:36:38 +0100

    moty

--- routes/course.js
+++ routes/course.js
@@ -146,7 +146,7 @@ router.put("/updatecourse/:id", async(req, res) => {
 })
 
 
-router.get('/searchcourse', (req, res, next) => {
+router.get('/searchmusic', (req, res, next) => {
     const thename = req.query.thename;
     Course.find({
         $or: [{
